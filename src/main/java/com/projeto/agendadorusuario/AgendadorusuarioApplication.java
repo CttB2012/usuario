835941660d9d8e3474c439f74b21aa5ace8301fc
CttB2012 <109867823+CttB2012@users.noreply.github.com>
2024-11-12 20:17:40 -0300

    inicial

---
+++ src/main/java/com/projeto/agendadorusuario/AgendadorusuarioApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.agendadorusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorusuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorusuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/agendadorusuario/AgendadorusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.agendadorusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
